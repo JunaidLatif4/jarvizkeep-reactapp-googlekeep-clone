a275d94cb94dd76ce6d4f869e9cd2e0d4853ce77
Junaid Latif <junaidlatif004@gmail.com>
2020-12-24 23:27:47 +0500

    Jarviz Keep App In React LIVE 1.0

---
+++ build/precache-manifest.2ac7a348150399a11b302c1653f2cc73.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c894794d85f4aedbb189747d3ab5823a",
+    "url": "/jarvizkeep-reactapp-googlekeep-clone/index.html"
+  },
+  {
+    "revision": "acdfd6ce696e485396dc",
+    "url": "/jarvizkeep-reactapp-googlekeep-clone/static/css/main.6851aa1d.chunk.css"
+  },
+  {
+    "revision": "67e86c19027cb6828c5b",
+    "url": "/jarvizkeep-reactapp-googlekeep-clone/static/js/2.45a78c45.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/jarvizkeep-reactapp-googlekeep-clone/static/js/2.45a78c45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "acdfd6ce696e485396dc",
+    "url": "/jarvizkeep-reactapp-googlekeep-clone/static/js/main.5e8f705c.chunk.js"
+  },
+  {
+    "revision": "75c2331a1ed69b5e8033",
+    "url": "/jarvizkeep-reactapp-googlekeep-clone/static/js/runtime-main.47dc990c.js"
+  },
+  {
+    "revision": "0a90c82df30ecc5d46bcbcf75c040bf7",
+    "url": "/jarvizkeep-reactapp-googlekeep-clone/static/media/keep.0a90c82d.png"
+  }
+]);
